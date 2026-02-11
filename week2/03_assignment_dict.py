6baa628f95482cc5cc270fb6cb5884c72898118d
Ali <asimae@udel.edu>
2026-02-11 10:38:23 -0500

    review dict

--- week2/03_assignment_dict.py
+++ week2/03_assignment_dict.py
@@ -1,8 +1,16 @@
 assignment = {
     "id": "HW1",
-    "title":"Homework1: Intro to Database Design",
+    "title" :  "Homework1: Intro to Database Design",
     "description": "Create an ER Model",
     "due_date": "2026-01-10",
     "available": False,
     "questions":[]
 }
+print(assignment)
+print("----")
+if "title" in assignment:
+    print(f"Title: {assignment["title"]}")
+
+assignment["additonal_notes"] = "basics of database" #add new key to the dictionary
+
+print(assignment)
